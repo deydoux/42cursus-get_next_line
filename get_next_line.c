5b3993ba2ef96d0c5814a508a25771975f6ff792
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-11-22 10:01:23 +0100

    Add init_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:02:10 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/22 01:17:32 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/22 10:01:07 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ static size_t	init_buffer(char **buffer)
 	size_t	len;
 
 	start = 0;
-	while (start < BUFFER_SIZE && *buffer[start] != '\n')
+	len = 0;
+	while (start < BUFFER_SIZE && *buffer[start] && *buffer[start] != '\n')
 		start++;
-	while (start + len < BUFFER_SIZE)
+	while (start + len < BUFFER_SIZE && *buffer[start + len])
 	{
 		buffer[len] = buffer[start + len];
 		len++;
@@ -28,38 +29,45 @@ static size_t	init_buffer(char **buffer)
 	return (len);
 }
 
-static int	find_newline(const char *s)
+static char	*init_line(size_t size)
 {
-	size_t	len;
-
-	while (len < BUFFER_SIZE && s[len] != '\n')
-		len++;
-	return (s[len] == '\n');
-}
-
-static char	*get_line(char **buffer, size_t size, int init)
-{
-	size_t	len;
 	char	*line;
 
-	if (init)
-		len = init_buffer(buffer);
-	if (find_newline(*buffer))
-	{
-		while (buffer[len] != '\n')
-			len--;
-		line = malloc(sizeof(char) * (size + len + 1));
-		line[size + len] = 0;
-	}
+	line = malloc(sizeof(char) * (++size + 1));
+	line[size--] = 0;
+	line[size--] = '\n';
+	return (line);
+}
 
+static char	*get_line(int fd, char **buffer, size_t size, int init)
+{
+	size_t	len;
+	size_t	buffer_len;
+	char	*line;
+
+	len = 0;
+	if (init)
+		buffer_len = init_buffer(buffer);
+	else
+		buffer_len = read(fd, *buffer, BUFFER_SIZE);
+	if (buffer_len < BUFFER_SIZE)
+		*buffer[buffer_len] = 0;
+	while (len < buffer_len && *buffer[len] && *buffer[len] != '\n')
+		len++;
+	if (!*buffer[len] && *buffer[len] == '\n')
+		line = init_line(size + len);
+	else
+		line = get_line(fd, buffer, size + len, 0);
+	while (len-- >= 0)
+		line[size + len] = buffer[len];
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE];
-	char		*line;
 
 	if (fd < 1 || BUFFER_SIZE < 1)
 		return (NULL);
-	return (get_line(&buffer, 0, 1));
+	return (get_line(fd, &buffer, 0, 1));
 }
