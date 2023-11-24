86a91342c97a373c4a2ed43d35ce50dc5dbfdb85
Dorian Eydoux <dorian.eydoux@icloud.com>
2023-11-24 14:34:20 +0100

    Refactor create_line to improve buffer handling

--- get_next_line.c
+++ get_next_line.c
@@ -6,71 +6,68 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:02:10 by deydoux           #+#    #+#             */
-/*   Updated: 2023/11/22 16:04:12 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/11/24 14:26:32 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static size_t	init_buffer(char **buffer)
+static size_t	init_buffer(char *buffer, char *stash)
 {
 	size_t	start;
 	size_t	len;
 
 	start = 0;
 	len = 0;
-	while (start < BUFFER_SIZE && *buffer[start] && *buffer[start] != '\n')
+	while (stash[start] && stash[start] != '\n')
 		start++;
-	while (start + len < BUFFER_SIZE && *buffer[start + len])
+	if (stash[start++] == '\n')
 	{
-		buffer[len] = buffer[start + len];
-		len++;
+		while (stash[start + len] && stash[start + len] != '\n')
+		{
+			buffer[len] = stash[start + len];
+			len++;
+		}
 	}
 	return (len);
 }
 
-static char	*init_line(size_t size)
+static char	*create_line(int fd, char *stash, size_t size, int init)
 {
-	char	*line;
-
-	line = malloc(sizeof(char) * (++size + 1));
-	line[size--] = 0;
-	line[size--] = '\n';
-	return (line);
-}
-
-static char	*get_line(int fd, char **buffer, size_t size, int init)
-{
-	size_t	len;
-	ssize_t	buffer_len;
+	ssize_t	len;
+	char	buffer[BUFFER_SIZE + 1];
 	char	*line;
 
 	len = 0;
 	if (init)
-		buffer_len = init_buffer(buffer);
-	else
-		buffer_len = read(fd, *buffer, BUFFER_SIZE);
-	if (buffer_len < 0)
+		len = init_buffer(buffer, stash);
+	if (!len)
+		len = read(fd, buffer, BUFFER_SIZE);
+	if (len <= 0)
 		return (NULL);
-	if (buffer_len < BUFFER_SIZE)
-		*buffer[buffer_len] = 0;
-	while (len < buffer_len && *buffer[len] && *buffer[len] != '\n')
+	buffer[len] = 0;
+	len = 0;
+	while (buffer[len] && buffer[len] != '\n')
 		len++;
-	if (!(buffer_len || init) || !*buffer[len] || *buffer[len] == '\n')
-		line = init_line(size + len);
+	len += buffer[len] == '\n';
+	if ((!init && len < BUFFER_SIZE && !buffer[len]) || buffer[len - 1] == '\n')
+	{
+		ft_memcpy(stash, buffer, BUFFER_SIZE);
+		line = malloc(sizeof(char) * (size + len + 1));
+	}
 	else
-		line = get_line(fd, buffer, size + len, 0);
+		line = create_line(fd, stash, size + len, 0);
 	if (line)
-		while (len-- > 0)
-			line[size + len] = *buffer[len];
+		while (len--)
+			line[size + len] = buffer[len];
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE];
+	static char	stash[BUFFER_SIZE + 1];
 
 	if (fd < 1 || BUFFER_SIZE < 1)
 		return (NULL);
-	return (get_line(fd, (char **)&buffer, 0, 1));
+	return (create_line(fd, stash, 0, 1));
 }
